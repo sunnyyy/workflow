899d0165a0a1061fcfdaf8e6805832e15012ef5c
Sunnia Ye <sye2@wellesley.edu>
2017-09-20 23:30:34 -0400

    gray shade change

--- js/calendar.js
+++ js/calendar.js
@@ -6,7 +6,7 @@ var formatPercent = d3.format(".1%");
 
 var color = d3.scaleQuantize()
     .domain([0, 10])
-    .range(["#ffffff","#f4b800","#e67d00","#e1002a","#b30159","#7d338c","#3163ac","#00a376","#7bb94e","#cccccc"]);
+    .range(["#ffffff","#f4b800","#e67d00","#e1002a","#b30159","#7d338c","#3163ac","#00a376","#7bb94e","#dddddd"]);
 
 var svg = d3.select("body")
     .selectAll("svg")
